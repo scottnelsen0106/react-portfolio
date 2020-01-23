ed93b15b62a7cb0774ce338d8a7252fea8a87d22
Scott Nelsen <snelsen0106@gmail.com>
2020-01-23 12:50:01 -0600

    got footer component to show up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/Navbar/NavBar'
+import NavBar from './components/Navbar/NavBar';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Footer/>
     </div>
   );
 }
